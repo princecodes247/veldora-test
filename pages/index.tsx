ff3b638360f4c1bc47f8047954825c46c4727823
Prince Codes <princecodes247@gmail.com>
2023-06-28 23:17:46 +0100

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,8 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
-import { WaitlistForm } from "@/components/WaitlistForm";
 import { Label } from "@/components/ui/label";
-import { useEffect, useRef, useState } from "react";
-import wordmarkLogo from "@/public/wordmark.png";
-import logo from "@/public/logo.png";
-import bg2 from "@/public/bg2.jpg";
+import {  useState } from "react";
 import {
   FacebookIcon,
   InstagramIcon,
@@ -57,70 +53,61 @@ export default function Home() {
     },
   });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    waitlistMutation.mutate({ email, name });
-  };
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   waitlistMutation.mutate({ email, name });
+  // };
 
-  const subscribersCount = useQuery({
-    queryKey: ["subscribers-count"],
-    queryFn: async () => {
-      const res = await getSubscribersCount();
-      setTargetCount(res.data.data);
-      return res.data.data;
-    },
-  });
+  // const subscribersCount = useQuery({
+  //   queryKey: ["subscribers-count"],
+  //   queryFn: async () => {
+  //     const res = await getSubscribersCount();
+  //     setTargetCount(res.data.data);
+  //     return res.data.data;
+  //   },
+  // });
   return (
     <>
       <main
         className={`flex relative min-h-screen text-white flex-col py-12 items-center font-body  px-2 md:px-8 ${inter.className}`}
       >
-        <Image
-          src={bg2}
-          className="absolute bg-fade inset-0 z-[-1] w-full object-cover h-full "
-          alt="Punteer Logo"
-        />
+  
         <Head>
           <title>
-            Punteer.com - Africa&apos;s first Punter&apos;s Social Platform
+            Veldora test
           </title>
           <meta
             name="description"
-            content="Curious about the next big thing in the betting industry? Want to get fast betting codes from your favourite punters through email, in-app alerts, then secure your spot now!"
+            content="Testing veldora"
           />
           <link rel="icon" href="/favicon.jpg" />
         </Head>
 
-        <Image
-          src={logo}
-          className="w-[180px] md:w-[200px] mb-2 md:mb-8 mx-auto"
-          alt="Punteer Logo"
-        />
+  
         <div
           className={clsx(
             isSubmitted ? "bg-black/40" : " bg-black/20",
             "max-w-[540px] waitlist-card backdrop-blur-sm text-white rounded-sm  w-full px-8 p-12"
           )}
         >
-          {/* <WaitlistForm /> */}
           {!isSubmitted ? (
             <>
               <h1 className="mb-4 text-3xl font-semibold text-center md:text-4xl font-heading">
-                Punteer is coming!
+                Veldora test
               </h1>
               <p className="my-4 text-sm text-center text-gray-300">
-                Curious about the next big thing in social media? Secure your
-                spot on our waitlist and be the first to explore our innovative
-                platform!
+                Try out veldora here
               </p>
 
               <p className="flex justify-center gap-2 my-4 mb-2 font-semibold text-center text-gray-200 ">
-                <UserCheck2 className="text-center text-green-400 " />
-                <span className="text-green-400">{subCount}</span> PEOPLE
-                WAITING
+                <UserCheck2 className="text-center text-green-400" />
+                <span className="text-green-400">{subCount}</span> TRIES
+                
               </p>
               <form
-                onSubmit={handleSubmit}
+                // onSubmit={handleSubmit}
+                method="POST"
+                action="https://veldora-server.onrender.com/api/v1/649b432a12ce587bdc84fdcd"
                 className="flex flex-col gap-2 mt-12"
               >
                 <div>
@@ -129,6 +116,7 @@ export default function Home() {
                     className="text-black rounded-sm bg-white/80"
                     id="email"
                     type="email"
+                    name="email"
                     placeholder="Email"
                     required
                     value={email}
@@ -141,6 +129,7 @@ export default function Home() {
                     className="text-black rounded-sm bg-white/80"
                     id="full-name"
                     type="text"
+                    name="full name"
                     placeholder="Full Name"
                     required
                     value={name}
@@ -148,36 +137,13 @@ export default function Home() {
                   />
                 </div>
                 <Button
-                  isLoading={waitlistMutation.isLoading}
+                  // isLoading={waitlistMutation.isLoading}
                   variant={"default"}
                   className="rounded-sm "
                 >
-                  Join the waitlist
+                  Test Form
                 </Button>
-                <div className="flex flex-col gap-4 p-2 px-4 mx-auto text-xs font-semibold bg-white md:flex-row text-primary">
-                  <Link
-                    href="mailto:info@punteer.com"
-                    className="flex items-center gap-2"
-                  >
-                    <span className="p-1 border rounded-full border-secondary text-secondary">
-                      <Mail size={14} />
-                    </span>
-                    <p>info@punteer.com</p>
-                  </Link>
-                  <div className="flex items-center gap-2">
-                    <span className="p-1 border rounded-full border-secondary text-secondary">
-                      <FacebookIcon size={14} />
-                    </span>
-                    <span className="p-1 border rounded-full border-secondary text-secondary">
-                      <InstagramIcon size={14} />
-                    </span>
-                    <span className="p-1 border rounded-full border-secondary text-secondary">
-                      <TwitterIcon size={14} />
-                    </span>
-
-                    <p>mypunteer</p>
-                  </div>
-                </div>
+           
               </form>
             </>
           ) : (
@@ -209,8 +175,7 @@ export default function Home() {
         </div>
       </main>
       <footer className="flex items-center justify-center gap-2 p-4 text-sm text-gray-700 bg-gray-100">
-        <Image src={wordmarkLogo} className="w-[100px]" alt="Punteer Logo" />
-        <p>© 2023 Punteer. All rights reserved</p>
+        <p>© 2023 Veldora. All rights reserved</p>
       </footer>
     </>
   );
